6358f231d2c8bed1e769d1fb93f14d997a1ac43c
fbg-91 <94171428+fbg-91@users.noreply.github.com>
2021-12-06 13:50:34 +0100

    Add files via upload

---
+++ Tipos.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        // Numéricos
+
+            // enteros
+
+        byte variable1 = 2;
+
+        short variable2 = 4;
+
+        int variable3 = 8;
+
+        long variable4 = 16;
+
+                // impresión de las variables de enteros
+
+        System.out.println(variable1 + " / " + variable2 + " / " + variable3 + " / " + variable4);
+
+            // decimales
+
+        float variable5 = 5.5f;
+
+        double variable6 = 10.90d;
+
+                // impresión de las variables decimales
+
+        System.out.println(variable5 + " / " + variable6);
+
+        // boolean
+
+        boolean variable7 = false;
+
+        boolean variable8 = true;
+
+                // impresión de las variables boolean
+
+        System.out.println(variable7 + " / " + variable8);
+
+        // texto
+
+        char variable9 = 'f';
+
+        String variable10 = "Fernando Barroso";
+
+                // impresión de las variables de texto
+
+        System.out.println(variable9 + " / " + variable10);
+
+    }
+}
