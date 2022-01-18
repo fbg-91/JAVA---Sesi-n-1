f17d2767c9598863d2d380b9cbeaa70697ee1e63
Fernando Barroso <94171428+fbg-91@users.noreply.github.com>
2022-01-18 09:44:48 +0100

    Add files via upload

---
+++ Bucle.java
@@ -0,0 +1,13 @@
+public class Bucle {
+
+    public static void main(String[] args) {
+
+
+        String[] nombres = {"Fer", "Ione", "Gema"};
+
+        for (int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+
+    }
+}
